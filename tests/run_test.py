b3ef29b289ab2b49d1f3ddac31bfcbcbadec9e0c
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2021-12-18 11:41:07 +0100

    use test result to terminate with exit code 1 if test failed

--- tests/run_test.py
+++ tests/run_test.py
@@ -122,4 +122,7 @@ if __name__ == '__main__':
         test_loader.loadTestsFromTestCase(TestContribution))
 
     runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(test_suite)
+    result = runner.run(test_suite)
+
+    if not result.wasSuccessful():
+        exit(1)
